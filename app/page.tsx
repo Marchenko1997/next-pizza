b09e79ce3db8fd9350a0a7f2550da18667cc1672
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-03-30 18:08:05 +0200

    set up product card

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Container, Filters, Title, TopBar } from "@/components/shared";
+import { ProductCard } from "@/components/shared/product-card";
 
 
 export default function Home() {
@@ -19,7 +20,12 @@ export default function Home() {
             <div className="flex flex-col gap-16">
               {/* <ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />
               <ProductsGroupList title="Комбо" items={[1, 2, 3, 4, 5]} /> */}
-              Список товаров
+              <ProductCard
+                id={1}
+                name="Пицца"
+                price={100}
+                imageUrl="https://media.dodostatic.net/image/r:584x584/01953ced168c758399984df8561623a3.avif"
+              />
             </div>
           </div>
         </div>
