f509bbd699e5df93c67e58d829bc1bcfcd0402ed
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-03-30 14:57:14 +0200

    finished header

---
+++ components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Button } from "./button";
+export { Input } from "./input";
+export { Select } from "./select";
+export { Slider } from "./slider";
+export { Checkbox } from "./checkbox";
+export { Skeleton } from "./skeleton";
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor } from "./popover";
+export { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogOverlay, DialogPortal, DialogTitle, DialogTrigger } from "./dialog";
+export { Drawer, DrawerPortal, DrawerOverlay, DrawerTrigger, DrawerClose, DrawerContent, DrawerHeader, DrawerFooter, DrawerTitle, DrawerDescription } from "./drawer";
