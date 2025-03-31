fc58ef9abe08e504ecccc5d74507ddb145c8786e
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-03-31 17:14:00 +0200

    updating down scroll

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <TopBar />
       {/* <div style={{ height: "3000px" }} /> */}
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           <div className="w-[250px]">
             <Filters />
           </div>
@@ -32,7 +32,7 @@ export default function Home() {
                     items: [{ price: 550 }],
                   },
                   {
-                    id: 1,
+                    id: 2,
                     name: "Чизбургер-пицца",
                     imageUrl:
                       "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
@@ -40,7 +40,7 @@ export default function Home() {
                     items: [{ price: 550 }],
                   },
                   {
-                    id: 1,
+                    id: 3,
                     name: "Чизбургер-пицца",
                     imageUrl:
                       "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
@@ -48,7 +48,7 @@ export default function Home() {
                     items: [{ price: 550 }],
                   },
                   {
-                    id: 1,
+                    id: 4,
                     name: "Чизбургер-пицца",
                     imageUrl:
                       "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
@@ -56,7 +56,7 @@ export default function Home() {
                     items: [{ price: 550 }],
                   },
                   {
-                    id: 1,
+                    id: 5,
                     name: "Чизбургер-пицца",
                     imageUrl:
                       "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
@@ -64,7 +64,7 @@ export default function Home() {
                     items: [{ price: 550 }],
                   },
                   {
-                    id: 1,
+                    id: 6,
                     name: "Чизбургер-пицца",
                     imageUrl:
                       "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
@@ -73,6 +73,60 @@ export default function Home() {
                   },
                 ]}
                 categoryId={1}
+              />
+              <ProductsGroupList
+                title={"Завтрак"}
+                items={[
+                  {
+                    id: 1,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61698827ee9b8db6d0aec53410.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={2}
               />
             </div>
           </div>
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import React from "react";
 import { Title } from "./title";
 import { cn } from "@/lib/utils";
 import { ProductCard } from "./product-card";
+import { useIntersection } from "react-use";
+import { useCategoryStore } from "@/store/category";
 
 interface Props {
   title: string;
@@ -11,21 +15,39 @@ interface Props {
   categoryId: number;
 }
 
-export const ProductsGroupList: React.FC<Props> = ({ title, items, className, listClassName, categoryId }) => {
-    return (
-      <div className={className}>
-        <Title text={title} size="lg" className="font-extrabold mb-5" />
-            <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
-                {items.map((product, i) => (
-                    <ProductCard
-                        key={product.id}
-                        id={product.id}
-                        name={product.name}
-                        imageUrl={product.imageUrl}
-                        price={product.items[0].price}
-                    />
-                ))}
-        </div>
+export const ProductsGroupList: React.FC<Props> = ({
+  title,
+  items,
+  className,
+  listClassName,
+  categoryId,
+}) => {
+    const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId);
+    }
+  }, [categoryId, intersection?.isIntersecting, title]);
+
+  return (
+    <div className={className} id={title} ref={intersectionRef}>
+      <Title text={title} size="lg" className="font-extrabold mb-5" />
+      <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
+        {items.map((product, i) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            imageUrl={product.imageUrl}
+            price={product.items[0].price}
+          />
+        ))}
       </div>
-    );
+    </div>
+  );
 };
