48b5ee612ff981fc98d4238d7f02f8f387d7a157
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-04-06 20:52:04 +0200

    set up custom hooks

---
+++ hooks/useFilterIngredients.ts
@@ -0,0 +1,12 @@
+import { Ingredient } from "@prisma/client";
+
+interface ReturnProps {
+  items: Ingredient[];
+}
+
+export const useFilterIngredients = (): ReturnProps => {
+
+    React.useEffect(() => {
+
+    },[])
+};
