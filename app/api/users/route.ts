26d7510ce80560861122ceef6770e2c87bda054c
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-04-06 18:11:12 +0200

    set up seed.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -9,6 +9,7 @@ export async function GET() {
 
 export async function POST(req: NextRequest) {
     const data = await req.json();
+
     const user = await prisma.user.create({ data })
     
     return NextResponse.json(user);
