838f29ea60182ff4692f25bbebf2c349b25d4dd2
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-05-25 18:13:27 +0200

    updating api categories

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -5,27 +5,28 @@ import { cn } from "@/lib/utils";
 import { useCategoryStore } from "@/store/category";
 
 interface Props {
+  items: Category[];
   className?: string;
 }
 
-const cats = [
-  { id: 1, name: "Пиццы" },
-  { id: 2, name: "Комбо" },
-  { id: 3, name: "Закуски" },
-  { id: 4, name: "Коктейли" },
-  { id: 5, name: "Кофе" },
-  { id: 6, name: "Напитки" },
-  { id: 7, name: "Десерты" },
-];
+// const cats = [
+//   { id: 1, name: "Пиццы" },
+//   { id: 2, name: "Комбо" },
+//   { id: 3, name: "Закуски" },
+//   { id: 4, name: "Коктейли" },
+//   { id: 5, name: "Кофе" },
+//   { id: 6, name: "Напитки" },
+//   { id: 7, name: "Десерты" },
+// ];
 
 
-export const Categories: React.FC<Props> = ({ className }) => {
+export const Categories: React.FC<Props> = ({ items, className }) => {
   const categoryActiveID = useCategoryStore((state) => state.activeId);
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
     >
-      {cats.map(({ name, id}, index) => (
+      {items.map(({ name, id}, index) => (
         <a
           className={cn(
             "flex items-center font-bold h-11 rounded-2xl px-5",
