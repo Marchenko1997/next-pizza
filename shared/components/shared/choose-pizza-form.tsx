eae5c5363313a4aa49fb24e0fb490a6ba62ba5be
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-05-28 19:19:04 +0200

    updating pizza sizes

--- shared/components/shared/choose-pizza-form.tsx
+++ shared/components/shared/choose-pizza-form.tsx
@@ -1,19 +1,28 @@
 import { cn } from "@/shared/lib/utils";
 import React from "react";
-import { PizzaImage} from "./pizza-image";
+import { PizzaImage } from "./pizza-image";
 import { Title } from "./title";
 import { Button } from "../ui";
 import { GroupVariants } from "./group-variants";
-import { PizzaSize, pizzaSizes, PizzaType, pizzaTypes } from "@/shared/constants/pizza";
+import {
+  mapPizzaType,
+  PizzaSize,
+  pizzaSizes,
+  PizzaType,
+  pizzaTypes,
+} from "@/shared/constants/pizza";
 import { IngredientItem } from "./ingredient-item";
 import { on } from "events";
-
+import { useSet } from "react-use";
+import { ProductWithRelations } from "@/@types/prisma";
+import { Ingredient, ProductItem } from "@prisma/client";
+import { calcTotalPizzaPrice } from "@/shared/lib/calc-total-pizza-price";
 
 interface Props {
   imageUrl: string;
   name: string;
   ingredients: Ingredient[];
-  items?: any[];
+  items: ProductItem[];
   onClickAdd?: VoidFunction;
   className?: string;
 }
@@ -26,14 +35,40 @@ export const ChoosePizzaForm: React.FC<Props> = ({
   onClickAdd,
   className,
 }) => {
-
   const [size, setSize] = React.useState<PizzaSize>(20);
   const [type, setType] = React.useState<PizzaType>(1);
-  
 
-  const textDetaills = "30 см, традиционное тесто 30";
-    const totalPrice = 350;
+  const [selectedIngredients, { toggle: addIngredient }] = useSet(
+    new Set<number>([])
+  );
 
+  const totalprice = calcTotalPizzaPrice(type, size, items, ingredients, selectedIngredients);
+
+  const textDetaills = `${size} см, ${mapPizzaType[type]} пицца `;
+
+  // const filteredPizzasByType = items.filter((item) => item.pizzaType === type);
+
+  // const availablePizzaSizes = pizzaSizes.map((item) => ({
+  //   name: item.name,
+  //   value: item.value,
+  //   disabled: !filteredPizzasByType.some(
+  //     (pizza) => Number(pizza.size) === Number(item.value)
+  //   ),
+  // }));
+
+  React.useEffect(() => {
+    const isAvailableSize = availablePizzaSizes?.find(
+      (item) => Number(item.value) === size && !item.disabled
+    );
+    const availableSize = availablePizzaSizes?.find((item) => !item.disabled);
+    if (!isAvailableSize && availableSize) {
+      setSize(Number(availableSize.value) as PizzaSize);
+    }
+  }, [type]);
+
+  const handleClickAdd = () => {
+    onClickAddCart?.();
+  };
 
   return (
     <div className={cn(className, "flex flex-1")}>
@@ -43,7 +78,7 @@ export const ChoosePizzaForm: React.FC<Props> = ({
         <p className="text-gray-400">{textDetaills}</p>
         <div className="flex flex-col gap-4 mt-5">
           <GroupVariants
-            items={pizzaSizes}
+            items={availablePizzaSizes}
             value={String(size)}
             onClick={(value) => setSize(Number(value) as PizzaSize)}
           />
@@ -52,20 +87,25 @@ export const ChoosePizzaForm: React.FC<Props> = ({
             value={String(type)}
             onClick={(value) => setType(Number(value) as PizzaType)}
           />
-          <div className="grid grid-cols-3 gap-3">
-            {ingredients.map((ingredient) => (
-              <IngredientItem
-                key={ingredient.id}
-                name={ingredient.name}
-                price={ingredient.price}
-                imageUrl={ingredient.imageUrl}
-                onClick={onClickAdd}
-        
-              />
-            ))}
+          <div className="bg-gray-50 p-5 rounded-md h-[420px] overflow-auto scrollbar mt-5">
+            <div className="grid grid-cols-3 gap-3">
+              {ingredients.map((ingredient) => (
+                <IngredientItem
+                  key={ingredient.id}
+                  name={ingredient.name}
+                  price={ingredient.price}
+                  imageUrl={ingredient.imageUrl}
+                  onClick={() => addIngredient(ingredient.id)}
+                  active={selectedIngredients.has(ingredient.id)}
+                />
+              ))}
+            </div>
           </div>
         </div>
-        <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
+        <Button
+          onClick={handleClickAdd}
+          className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10"
+        >
           Добавить в корзину за {totalPrice} ₽
         </Button>
       </div>
