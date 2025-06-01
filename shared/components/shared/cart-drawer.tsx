004d9f6036546aac0bdd29dd15158d5e31eb494a
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-06-01 09:12:17 +0200

    side modal item create

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   Sheet,
@@ -11,7 +13,8 @@ import {
 import Link from "next/link";
 import { Button } from "../ui";
 import { ArrowRight } from "lucide-react";
-
+import { CartDrawerItem } from "./cart-drawer-item";
+import { getCartItemDetails } from "@/shared/lib";
 
 interface Props {
   className?: string;
@@ -27,8 +30,24 @@ export const CartDrawer: React.FC<React.PropsWithChildren> = ({ children }) => {
             В корзине <span className="font-bold">3 товара</span>
           </SheetTitle>
         </SheetHeader>
+        <div className="-mx-6 mt-5 overflow-auto flex-1">
+          <div className="mb-2">
+            <CartDrawerItem
+              id={1}
+              imageUrl={
+                "https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp"
+              }
+              details={getCartItemDetails(2, 30, [
+                { name: "Цыпленок" },
+                { name: "Сыр" },
+              ])}
+              name={"Чоризо фреш"}
+              price={419}
+              quantity={1}
+            />
+          </div>
+        </div>
 
-        {/* Items */}
         <SheetFooter className="-mx-6 bg-white p-8">
           <div className="w-full">
             <div className="flex mb-4">
@@ -40,11 +59,7 @@ export const CartDrawer: React.FC<React.PropsWithChildren> = ({ children }) => {
               <span className="font-bold text-lg">500 ₽</span>
             </div>
             <Link href="/checkout">
-              <Button
-            
-                type="submit"
-                className="w-full h-12 text-base"
-              >
+              <Button type="submit" className="w-full h-12 text-base">
                 Оформить заказ
                 <ArrowRight className="w-5 ml-2" />
               </Button>
