61095b08b01ffbca62e934874cebbdaf2d37a11b
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-06-25 14:26:08 +0200

    set up forms for purchase page

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,12 +1,43 @@
-import { Container, Title } from "@/shared/components/shared";
+import { Container, Title, WhiteBlock } from "@/shared/components/shared";
+import { Input, Textarea } from "@/shared/components/ui";
 
 export default function Checkout() {
-    return (
-      <Container className="mt-10">
-        <Title
-          text="Оформление заказа"
-          className="font-extrabold mb-8 text-[36px]"
-        />
-      </Container>
-    );
+  return (
+    <Container className="mt-10">
+      <Title
+        text="Оформление заказа"
+        className="font-extrabold mb-8 text-[36px]"
+      />
+      <div className="flex gap-10">
+        {/* Левая часть */}
+        <div className="flex flex-col gap-10 flex-1 mb-20">
+          <WhiteBlock title="1.Корзина"></WhiteBlock>
+          <WhiteBlock title="1.Персонаьные данные">
+            <div className="grid grid-cols-2 gap-5">
+              <Input name="firstName" className="text-base" placeholder="Имя" />
+              <Input
+                name="lastName"
+                className="text-base"
+                placeholder="Фамилия"
+              />
+              <Input name="email" className="text-base" placeholder="E-Mail" />
+              <Input name="phone" className="text-base" placeholder="Телефон" />
+            </div>
+          </WhiteBlock>
+          <WhiteBlock title="3.Адресс доставки">
+            <div className="flex flex-col gap-5">
+              <Input name="firstName" className="text-base" placeholder="Адрес" />
+              <Textarea
+                className="text-base"
+                placeholder="Комментарий к заказу"
+                rows={5}
+              />
+            </div>
+          </WhiteBlock>
+        </div>
+        {/* Правая часть */}
+        <div className="w-[450px]">5665751572</div>
+      </div>
+    </Container>
+  );
 }
--- shared/components/shared/index.ts
+++ shared/components/shared/index.ts
@@ -17,4 +17,5 @@ export { IngredientItem } from "./ingredient-item";
 export { CartButton } from "./cart-button";
 export { CartDrawer } from "./cart-drawer";
 export { ProductForm } from "./product-form";
+export { WhiteBlock } from "./white-block";
 export * from "./modals";
