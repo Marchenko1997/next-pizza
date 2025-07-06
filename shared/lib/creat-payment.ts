06bc51fd1111092b1d0b85fe1eeb159e7992561e
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-07-06 21:26:28 +0200

    set up hpp response from Youkassa

--- shared/lib/creat-payment.ts
+++ shared/lib/creat-payment.ts
@@ -27,11 +27,11 @@ export async function createPayment(details: Props) {
     },
     {
       auth: {
-        username: process.env.YOOKASSA_API_KEY as string,
-        password: "",
+        username: process.env.YOOKASSA_STORE_ID as string,
+        password: process.env.YOOKASSA_API_KEY as string,
       },
       headers: {
-        //   "Content-Type": "application/json",
+        "Content-Type": "application/json",
         "Idempotence-Key": Math.random().toString(36).substring(7),
       },
     }
