34b24cecb3a72c79b77a1145065ef52737e349f3
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-07-27 20:21:22 +0200

    updating next types

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -3,19 +3,15 @@ import { ChooseProductModal } from "@/shared/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
 
-
-interface ProductParams {
-  params: {
-    id: string;
-  };
-}
-
-export default async function ProductModalPage({ params }: ProductParams) {
+export default async function ProductModalPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const product = await prisma.product.findFirst({
     where: { id: Number(params.id) },
     include: { ingredients: true, items: true },
   });
-
   if (!product) return notFound();
   return <ChooseProductModal product={product} />;
 }
