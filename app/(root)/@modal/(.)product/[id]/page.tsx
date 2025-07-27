3fdcd7f8a76c8de0d59fa17ad68b955f3deb5745
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-07-27 19:37:25 +0200

    updating eslint mistakes

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -2,13 +2,11 @@ import { ChooseProductModal } from "@/shared/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
 
-type ProductPageProps = {
-  params: {
-    id: string;
-  };
-};
-
-export default async function ProductModalPage({ params }: ProductPageProps) {
+export default async function ProductModalPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const product = await prisma.product.findFirst({
     where: { id: Number(params.id) },
     include: { ingredients: true, items: true },
