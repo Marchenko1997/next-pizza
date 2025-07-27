8a921e1acd0a5cb228507af04295910909f7241b
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-07-27 20:31:25 +0200

    updating types

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,10 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-
 import { ChooseProductModal } from "@/shared/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
 
-export default async function ProductModalPage({ params }: any) {
+export default async function ProductModalPage(props: unknown) {
+  const { params } = props as { params: { id: string } };
+
   const product = await prisma.product.findFirst({
     where: { id: Number(params.id) },
     include: { ingredients: true, items: true },
