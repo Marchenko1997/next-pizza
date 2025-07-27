536a3d4f6a0911b6d3f479f1bc0506867870f6ca
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-07-27 20:28:16 +0200

    updating product modal page

--- @types/next.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "next" {
-  export interface PageProps {
-    params: Record<string, string | string[]>;
-    searchParams?: Record<string, string | string[]>;
-  }
-}
--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,17 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 
 import { ChooseProductModal } from "@/shared/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
 
-export default async function ProductModalPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function ProductModalPage({ params }: any) {
   const product = await prisma.product.findFirst({
     where: { id: Number(params.id) },
     include: { ingredients: true, items: true },
   });
+
   if (!product) return notFound();
   return <ChooseProductModal product={product} />;
 }
