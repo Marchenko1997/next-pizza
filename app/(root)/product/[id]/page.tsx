da174131f7d1e9ebe42636cf871926c2eca8a7e8
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-07-27 20:54:14 +0200

    updating product page

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -3,13 +3,10 @@ import { Container, ProductForm } from "@/shared/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
 
-interface ProductPageProps {
-  params: {
-    id: string;
-  };
-}
 
-export default async function ProductPage({ params }: ProductPageProps) {
+
+export default async function ProductPage(props: unknown) {
+  const { params } = props as { params: { id: string } };
   const product = await prisma.product.findFirst({
     where: { id: Number(params.id) },
     include: {
